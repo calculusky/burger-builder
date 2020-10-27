05521aae6dadc780feb220369ccf4318200e29a6
calculusky <calculusky@gmail.com>
2020-10-27 20:05:15 +0100

    a

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxilliary/Auxiliary';
+
+class BurgerBuilder extends Component {
+
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Burger Control</div>
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder;
