068a6eb6bba312d8c80a45bf31b872278c4f27fc
calculusky <calculusky@gmail.com>
2020-11-03 14:24:16 +0100

    burger2

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './BurgerIngredient.module.css';
+const BurgerIngredient = (props) => {
+    return (
+    <div>
+        
+    </div>
+    );
+}
+export default BurgerIngredient;
